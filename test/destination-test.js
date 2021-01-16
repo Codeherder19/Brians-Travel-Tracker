e9fc1afbed06d930d2ae0140d969d3dc02aeafff
Brian Forbes <brianandrewf81@gmail.com>
2021-01-16 15:16:27 -0700

    Test destinations class for id and destination name

--- test/destination-test.js
+++ test/destination-test.js
@@ -1,14 +1,33 @@
 import { expect } from 'chai';
 
-import Destination from '../src/user';
+import Destination from '../src/Destination';
 
 import { destinations } from './test-data';
 
 describe('Destinations', () => {
   let tripDestinations;
+  let trip1;
+  let trip2;
 
   beforeEach(() => {
      tripDestinations = destinations.destinations.map(destination => new Destination(destination));
-
+     trip1 = tripDestinations[0];
+     trip2 = tripDestinations[7];
   });
+
+  it('should be a function', () => {
+    expect(Destination).to.be.a('function');
+  });
+
+  it('each destination should have an id', () => {
+    expect(trip1.id).to.eq(1);
+    expect(trip2.id).to.eq(8);
+  })
+
+  it('each destination should have a destination name', () => {
+    expect(trip1.location).to.eq("Lima, Peru");
+    expect(trip2.location).to.eq("Tokyo, Japan");
+  })
+
+  // it('each destination should have a ')
   });
